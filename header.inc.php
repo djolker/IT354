5a98c768be568acfea587fce2697b9dccbb5a12f
DJ Olker <dj.olker@gmail.com>
2020-02-03 19:13:06 -0600

    Adding first additions to site

---
+++ header.inc.php
@@ -0,0 +1,17 @@
+<header>
+  <div id="topHeader">
+    <div class="container">
+      <nav class="navbar" role="navigation">
+        <div id="logoRow" >
+           <div class="container">
+              <div class="row">
+                 <div class="col-md-12 panel" style="background-color:#8cf5b9;">
+                     <h1>DJ Olker IT 354</h1>
+                 </div>
+              </div>
+            </div>
+          </div>
+      </nav>
+    </div>
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>DJ Olker Assignment Repo</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <br>
+
+<?php include 'header.inc.php' ?>
+
+    <div class="container">
+      <div class="fill col-md-12">
+      <img src="IMG_2044.jpeg" >
+    </div>
+      <div class="row">
+        <div class="col-md-3 panel" style="background-color:#8cc25e;">
+          <h3>Bio</h3>
+          Name: DJ Olker<br>
+          Major: Information Systems<br>
+          Year: Senior <br><br>
+        </div>
+        <div class="col-md-3 panel" style="background-color:#8cc25e;">
+          <br>
+          <blockquote>The purpose of this website is to server a series of examples based on the curriculum of IT 354.</blockquote>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
